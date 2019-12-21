145682d0e85e544710787e7d3d6502c7b6246db4
AlexSpeicher <alexanderspeicher@gmail.com>
2019-12-21 11:25:56 -0500

    Added single shader with texture and color

--- Nucleus/src/Nucleus/Renderer/Renderer2D.cpp
+++ Nucleus/src/Nucleus/Renderer/Renderer2D.cpp
@@ -13,8 +13,8 @@ namespace Nucleus {
 	struct Renderer2DStorage
 	{
 		Ref<VertexArray> SquareVertexArray;
-		Ref<Shader> FlatColorShader;
 		Ref<Shader> TextureShader;
+		Ref<Texture2D> WhiteTexture;
 	};
 
 	static Renderer2DStorage* s_Data;
@@ -48,8 +48,10 @@ namespace Nucleus {
 		m_SquareIndexBuffer.reset(IndexBuffer::Create(m_Squareindices, sizeof(m_Squareindices) / sizeof(uint32_t)));
 		s_Data->SquareVertexArray->SetIndexBuffer(m_SquareIndexBuffer);
 
+		s_Data->WhiteTexture = Texture2D::Create(1, 1);
+		uint32_t whiteTextureData = 0xffffffff;
+		s_Data->WhiteTexture->SetData(&whiteTextureData, sizeof(uint32_t));
 
-		s_Data->FlatColorShader = Shader::Create("assets/shaders/FlatColor.glsl");
 		s_Data->TextureShader = Shader::Create("assets/shaders/Texture.glsl");
 		s_Data->TextureShader->Bind();
 		s_Data->TextureShader->SetInt("u_texture", 0);
@@ -62,10 +64,6 @@ namespace Nucleus {
 
 	void Renderer2D::BeginScene(const OrthographicCamera& camera)
 	{
-		s_Data->FlatColorShader->Bind();
-		s_Data->FlatColorShader->SetMat4("u_ViewProjection", camera.GetViewProjectionMatrix());
-
-
 		s_Data->TextureShader->Bind();
 		s_Data->TextureShader->SetMat4("u_ViewProjection", camera.GetViewProjectionMatrix());
 	}
@@ -81,13 +79,14 @@ namespace Nucleus {
 
 	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4 color, const float rotation)
 	{
-		s_Data->FlatColorShader->Bind();
-		s_Data->FlatColorShader->SetFloat4("u_Color", color);
+		s_Data->TextureShader->SetFloat4("u_Color", color);
+		s_Data->WhiteTexture->Bind();
+		// Bind white texture 
 
 		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * 
 			glm::rotate(glm::mat4(1.0f), rotation, glm::vec3(0.0f, 0.0f, 1.0f)) *
 			glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
-		s_Data->FlatColorShader->SetMat4("u_Transform", transform);
+		s_Data->TextureShader->SetMat4("u_Transform", transform);
 
 		s_Data->SquareVertexArray->Bind();
 		RenderCommand::DrawIndexed(s_Data->SquareVertexArray);
@@ -101,6 +100,7 @@ namespace Nucleus {
 
 	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D>& texture, const float rotation)
 	{
+		s_Data->TextureShader->SetFloat4("u_Color", glm::vec4(1.0f));
 		s_Data->TextureShader->Bind();
 
 		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) *
--- Sandbox/assets/shaders/Texture.glsl
+++ Sandbox/assets/shaders/Texture.glsl
@@ -23,8 +23,9 @@ layout(location = 0) out vec4 a_Color;
 
 in vec2 v_TexCoord;
 
+uniform vec4 u_Color;
 uniform sampler2D u_Texture;
 
 void main() {
-	a_Color = texture(u_Texture, v_TexCoord) * vec4(1.0, 0.8, 0.8, 1.0);
+	a_Color = texture(u_Texture, v_TexCoord) * u_Color;
 }
