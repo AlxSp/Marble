99e2b485e31f29d83b24f742b3411d2c5fe040c4
Alex Speicher <alexanderspeicher@gmail.com>
2020-04-16 20:14:52 -0400

    Improved Instrumentor based on Hazel repo

--- Nucleus/src/Nucleus/Debug/Instrumentor.h
+++ Nucleus/src/Nucleus/Debug/Instrumentor.h
@@ -18,16 +18,19 @@
 #include <chrono>
 #include <algorithm>
 #include <fstream>
-
+#include <iomanip>
 #include <thread>
 
 namespace Nucleus {
 
+    using FloatingPointMicroseconds = std::chrono::duration<double, std::micro>;
+
     struct ProfileResult
     {
         std::string Name;
-        long long Start, End;
-        uint32_t ThreadID;
+        FloatingPointMicroseconds Start;
+        std::chrono::microseconds ElapsedTime;
+        std::thread::id ThreadID;
     };
 
     struct InstrumentationSession
@@ -38,55 +41,82 @@ namespace Nucleus {
     class Instrumentor
     {
     private:
+        std::mutex m_Mutex;
         InstrumentationSession* m_CurrentSession;
         std::ofstream m_OutputStream;
-        int m_ProfileCount;
+
     public:
         Instrumentor()
-            : m_CurrentSession(nullptr), m_ProfileCount(0)
+            : m_CurrentSession(nullptr)
         {
         }
 
         void BeginSession(const std::string& name, const std::string& filepath = "results.json")
         {
+            std::lock_guard lock(m_Mutex);
+            if(m_CurrentSession){
+                // If there is already a current session, then close it before beginning new one.
+				// Subsequent profiling output meant for the original session will end up in the
+				// newly opened session instead.  That's better than having badly formatted
+				// profiling output.
+                if (Log::GetCoreLogger()) {
+                    NC_CORE_ERROR("Instrumentor::BeginSession('{0}') when session '{1}' already open.", name, m_CurrentSession->Name);
+                }
+                InternalEndSession();
+            }
+
             m_OutputStream.open(filepath);
-            WriteHeader();
-            m_CurrentSession = new InstrumentationSession{ name };
+
+            if(m_OutputStream.is_open()){
+                m_CurrentSession = new InstrumentationSession({name});
+                WriteHeader();
+            } else {
+                if (Log::GetCoreLogger()) {
+                    NC_CORE_ERROR("Instrumentor could not open results file '{0}'.", filepath);
+                } 
+            }
         }
 
         void EndSession()
         {
-            WriteFooter();
-            m_OutputStream.close();
-            delete m_CurrentSession;
-            m_CurrentSession = nullptr;
-            m_ProfileCount = 0;
+            std::lock_guard lock(m_Mutex);
+            InternalEndSession();
         }
 
         void WriteProfile(const ProfileResult& result)
         {
-            if (m_ProfileCount++ > 0)
-                m_OutputStream << ",";
+            std::stringstream json;
 
             std::string name = result.Name;
             std::replace(name.begin(), name.end(), '"', '\'');
 
-            m_OutputStream << "{";
+            m_OutputStream << ",{";
             m_OutputStream << "\"cat\":\"function\",";
-            m_OutputStream << "\"dur\":" << (result.End - result.Start) << ',';
+            m_OutputStream << "\"dur\":" << (result.ElapsedTime.count()) << ',';
             m_OutputStream << "\"name\":\"" << name << "\",";
             m_OutputStream << "\"ph\":\"X\",";
             m_OutputStream << "\"pid\":0,";
             m_OutputStream << "\"tid\":" << result.ThreadID << ",";
-            m_OutputStream << "\"ts\":" << result.Start;
+            m_OutputStream << "\"ts\":" << result.Start.count();
             m_OutputStream << "}";
 
-            m_OutputStream.flush();
+            std::lock_guard lock(m_Mutex);
+            if (m_CurrentSession) {
+                m_OutputStream << json.str();
+                m_OutputStream.flush();
+            }
         }
+
+        static Instrumentor& Get() {
+            static Instrumentor instance;
+            return instance;
+        }
+
+    private:
 
         void WriteHeader()
         {
-            m_OutputStream << "{\"otherData\": {},\"traceEvents\":[";
+            m_OutputStream << "{\"otherData\": {},\"traceEvents\":[{}";
             m_OutputStream.flush();
         }
 
@@ -96,10 +126,14 @@ namespace Nucleus {
             m_OutputStream.flush();
         }
 
-        static Instrumentor& Get()
-        {
-            static Instrumentor instance;
-            return instance;
+        //Note: must already own lock on m_mutex before calling InternalEndSession()
+        void InternalEndSession(){
+            if (m_CurrentSession) {
+                WriteFooter();
+                m_OutputStream.close();
+                delete m_CurrentSession;
+                m_CurrentSession = nullptr;
+            }
         }
     };
 
@@ -109,7 +143,7 @@ namespace Nucleus {
         InstrumentationTimer(const char* name)
             : m_Name(name), m_Stopped(false)
         {
-            m_StartTimepoint = std::chrono::high_resolution_clock::now();
+            m_StartTimepoint = std::chrono::steady_clock::now();
         }
 
         ~InstrumentationTimer()
@@ -120,31 +154,50 @@ namespace Nucleus {
 
         void Stop()
         {
-            auto endTimepoint = std::chrono::high_resolution_clock::now();
+            auto endTimepoint = std::chrono::steady_clock::now();
 
-            long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
-            long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
+            auto highResStart = FloatingPointMicroseconds(m_StartTimepoint.time_since_epoch());
+            auto elapsedTime = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch() - std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch();
 
-            uint32_t threadID = std::hash<std::thread::id>{}(std::this_thread::get_id());
-            Instrumentor::Get().WriteProfile({ m_Name, start, end, threadID });
-
+            Instrumentor::Get().WriteProfile({ m_Name, highResStart, elapsedTime, std::this_thread::get_id() });
             m_Stopped = true;
         }
     private:
         const char* m_Name;
-        std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimepoint;
+        std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
         bool m_Stopped;
     };
 
 }
 
-#define NC_PROFILE 0
+#define NC_PROFILE 1
 
 #if NC_PROFILE
+    // Resolve which function signature macro will be used. Note that this only
+	// is resolved when the (pre)compiler starts, so the syntax highlighting
+	// could mark the wrong one in your editor!
+	#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+		#define NC_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__DMC__) && (__DMC__ >= 0x810)
+		#define NC_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__FUNCSIG__)
+		#define NC_FUNC_SIG __FUNCSIG__
+	#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+		#define NC_FUNC_SIG __FUNCTION__
+	#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+		#define NC_FUNC_SIG __FUNC__
+	#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+		#define NC_FUNC_SIG __func__
+	#elif defined(__cplusplus) && (__cplusplus >= 201103)
+		#define NC_FUNC_SIG __func__
+	#else
+		#define NC_FUNC_SIG "NC_FUNC_SIG unknown!"
+	#endif
+
     #define NC_PROFILE_BEGIN_SESSION(name, filepath) ::Nucleus::Instrumentor::Get().BeginSession(name, filepath)
     #define NC_PROFILE_END_SESSION() ::Nucleus::Instrumentor::Get().EndSession()
     #define NC_PROFILE_SCOPE(name) ::Nucleus::InstrumentationTimer timer##__LINE__(name);
-    #define NC_PROFILE_FUNCTION() NC_PROFILE_SCOPE (__FUNCSIG__) //void __cdecl Nucleus::Instrumentor::Stop(void)
+    #define NC_PROFILE_FUNCTION() NC_PROFILE_SCOPE (NC_FUNC_SIG) //void __cdecl Nucleus::Instrumentor::Stop(void)
 #else
     #define NC_PROFILE_BEGIN_SESSION(name, filepath)
     #define NC_PROFILE_END_SESSION()
